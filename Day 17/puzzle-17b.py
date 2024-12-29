6dbe01b5a2007266560e55d97eac39d542de887c
Aron Wallaker <aron.wallaker@gmail.com>
2024-12-29 14:41:50 -0500

    WIP

--- Day 17/puzzle-17b.py
+++ Day 17/puzzle-17b.py
@@ -1,7 +1,6 @@
 import sys
 import re
 
-
 debug = False
 
 def dprint(fs):
@@ -22,7 +21,7 @@ def read_data_file(fname):
         elif 'Program' in l:
             instructions = [int(i) for i in l.removeprefix('Program: ').split(',')]
 
-    return registers, instructions
+    return registers, instructions, l.removeprefix('Program: ')
 
 def process_operand(op,registers):
     if op in [0,1,2,3]:
@@ -39,60 +38,82 @@ def process_instruction(ins,opc,registers):
     combo = process_operand(opc,registers)
     if ins == 0:
         registers['A'] = int( registers['A'] / pow(2,combo) )
-        # dprint(f"adv {combo} -> {registers} ({pow(2,combo)})")
+        dprint(f"adv {combo} -> {registers} ({pow(2,combo)})")
     elif ins == 1:
-        registers['B'] ^= opc
-        # dprint(f"bxl {opc}  -> {registers}")
+        registers['B'] = registers['B']^opc
+        dprint(f"bxl {opc}  -> {registers}")
     elif ins == 2:
         registers['B'] = combo % 8
-        # dprint(f"bst {combo} -> {registers}")
+        dprint(f"bst {combo} -> {registers}")
     elif ins == 3:
         if registers['A'] != 0:
             jump = opc
-        # dprint(f"jmp ({opc}): {jump}")
+        dprint(f"jmp ({opc}): {jump}")
     elif ins == 4:
-        registers['B'] ^= registers['C']
-        # dprint(f"bxc  -> {registers}")
+        registers['B'] = registers['B']^registers['C']
+        dprint(f"bxc  -> {registers}")
     elif ins == 5:        
         output = str(combo % 8)
-        # dprint(f"out {output} -> {registers}")
+        dprint(f"out {output} -> {registers}")
     elif ins == 6:
         registers['B'] = int( registers['A'] / pow(2,combo) )
-        # dprint(f"bdv {combo} -> {registers}")
+        dprint(f"bdv {combo} -> {registers}")
     elif ins == 7:
         registers['C'] = int( registers['A'] / pow(2,combo) )
-        # dprint(f"cdv {combo} -> {registers}")
+        dprint(f"cdv {combo} -> {registers}")
 
 
     return output, jump
 
-if __name__ == '__main__':
-    button_costs = [3,1]
-    print(f"*** Day 17, Part 1 ***\n")
-    if(len(sys.argv) >=3 and sys.argv[2] == 'debug'):
-        debug = True
-    registers, instructions = read_data_file(sys.argv[1])
-    dprint(f"Registers: {registers}")
-    dprint(f"Program: {instructions}")
+def run_program(instructions,registers):
     output = ''
     i = 0
     while i < len(instructions):
         ins = instructions[i]
         opc = instructions[i+1]
-        preva = registers['A']
-        prevb = registers['B']
-        prevc = registers['C']
         out,jump = process_instruction(ins,opc,registers)
-        if out!='':
-            dprint(f"OUT: {out} vs A: {preva%8}, {prevb%8}, {prevc%8} -> ({registers})")
 
         if out != '':            
             if output != '':
                 output += ','
             output += out
+            if not program.startswith(output):
+                break                        
         if jump !=-1:
             i = jump
         else:
             i += 2
-    print(f"Output: {output}")
-    dprint(f"Final registers: {registers}")
+    return output
+    
+if __name__ == '__main__':
+    button_costs = [3,1]
+    print(f"*** Day 17, Part 2 ***\n")
+    if(len(sys.argv) >=3 and sys.argv[2] == 'debug'):
+        debug = True
+    registers, instructions, program = read_data_file(sys.argv[1])
+    dprint(f"Registers: {registers}")
+    dprint(f"Program: {instructions}")
+    dprint(f"Target: ({program})")
+
+    # end = 140737488355328 # 2**47
+    # end = 140737486962038
+    end = 105553114176561
+    start = 70368744177664 # 2**46
+    mol = 0
+    solution = -1
+    for i in range(end,start,-1):
+        reg = {}
+        reg['A'] = i
+        reg['B'] = registers['B']
+        reg['C'] = registers['C']
+        output = run_program(instructions,reg,program)
+        if len(output) > mol:
+            print(f">{i} ({output})-{len(output)} vs ({program})-{len(program)}")
+            mol = len(output)
+
+        if output == program:
+            print(f">>> {i}")
+            solution = i
+            break
+
+    print(f"Solution: {solution}")
